8b6b73653e5be70c1ae0940533f91eb44ac1a820
wongnoubo <1012084607@qq.com>
2019-02-22 23:17:37 +0800

    修改监听端口，自适应ganache-cli端口；给合约构造函数设置参数

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var Adoption = artifacts.require("Store");
+var Store = artifacts.require("Store");
 
 module.exports = function(deployer) {
-    deployer.deploy(Store);
+    deployer.deploy(Store,100000, web3.utils.toWei('0.1', 'ether'));
 };
